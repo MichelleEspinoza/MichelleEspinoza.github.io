ab14721cd86f38dede281b6cd9f35eacbd6f34bc
Michelle Espinoza <memichelle473@gmail.com>
2023-04-12 11:51:59 -0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,14 +3,14 @@ export default defineNuxtConfig({
     app: {
         baseURL: '/MichelleEspinoza.github.io/'
     },
-    runtimeConfig: {
+    /*runtimeConfig: {
       // Private config that is only available on the server
       apiSecret: '123',
       // Config within public will be also exposed to the client
       public: {
         apiBase: '/api'
       }
-    },
+    },*/
     css: ['~/assets/css/main.css'],
     postcss: {
       plugins: {
