b209715ab1cffcec779c90bebff2ba31d161f215
MichelleEspinoza <michelle@socalstudio.com>
2023-04-13 09:45:05 -0700

    updated changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="hero" class="bg-bluegray laptop:px-14 py-24 xl:px-[80px] max-[315px]:mr-[-34px]">
-      Test
+      Test 02
       <div class="flex justify-start space-x-4 mb-14">
           <button class="rounded-md bg-blue text-white px-4 py-2">
             Get Started
