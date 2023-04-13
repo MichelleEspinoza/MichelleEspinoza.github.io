1e5a329a28b4e1d3f1ebb5acafb6f9f5c4803a88
MichelleEspinoza <michelle@socalstudio.com>
2023-04-13 14:59:50 -0700

    updated SSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
+  ssr: false,
+  
+  nitro: { compressPublicAssets: true },
+  imports: {
+    dirs: ['stores'],
+  },
+    /*app: {
         baseURL: '/MichelleEspinoza.github.io/'
-    },
-    css: ['~/assets/css/main.css'],
+    },*/
+    css: ['@/assets/css/main.css'],
     modules: [
       '@nuxtjs/tailwindcss',
     ],
