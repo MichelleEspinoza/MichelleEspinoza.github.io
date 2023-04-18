b5ae7a1cc6c95feffd7d4cc407b91368dabdecbe
MichelleEspinoza <michelle@socalstudio.com>
2023-04-18 15:55:41 -0700

    add animation

---
+++ swiper.js
@@ -0,0 +1,21 @@
+var swiperW = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  }
+  );
+  var swiperD = new Swiper(".mySwiperDevice", {
+    direction: "horizontal",
+    slidesPerView: 1.3,
+    spaceBetween: 0,
+    mousewheel: true,
+    pagination: {
+      el: ".swiper-pagination-device",
+      clickable: true,
+    },
+  });
---
+++ tailwind.js
@@ -0,0 +1,76 @@
+
+tailwind.config = {
+  title: 'Michelle Espinoza - Portafolio',
+  theme: {
+      extend: {
+          keyframes: {
+              typing: {
+              "0%": {
+                  width: "0%",
+                  visibility: "hidden"
+              },
+              "100%": {
+                  width: "100%"
+              }  
+              },
+              blink: {
+              "50%": {
+                  borderColor: "transparent"
+              },
+              "100%": {
+                  borderColor: "white"
+              }  
+              }
+          },
+          animation: {
+              typing: "typing 2s steps(20) infinite alternate, blink .7s infinite"
+          },
+          colors: {
+              purple: '#5f1ec4',
+              pink: '#e41a92',
+              orange: '#ff7a01',
+              lesspink: '#f07995',
+              yellow: '#ffd600',
+              blue: '#01d3f6'
+          },
+          minHeight: {
+              20: '20px'
+          }
+      },
+      screens: {
+          sm: '480px',
+          md: '768px',
+          lg: '976px',
+          laptop: '1024px',
+          xl: '1440px'
+      },
+      fontFamily: {
+          Poppins: ['Roboto', 'sans-serif']
+      },
+      fontSize: {
+          sm: ['14px', '24px'],
+          base: ['18px', '24px'],
+          lg: ['22px', '27px'],
+          xl: ['28px', '32px'],
+          xxl: ['38px', '45px']
+      }
+  },
+}
+
+function reveal() {
+    const reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
