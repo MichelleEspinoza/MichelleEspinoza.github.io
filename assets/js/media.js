e68d6715cc31e80ad5ce3aae483a4787d2c1a0da
MichelleEspinoza <memichelle473@gmail.com>
2026-05-25 23:24:29 -0600

    default theme

--- assets/js/media.js
+++ assets/js/media.js
@@ -1,14 +1,13 @@
-/*Theme*/
+/* Theme */
 const storage = localStorage.getItem('darkMode');
 
-const isDark = (storage ? storage === 'true' : 'false');
+const isDark = storage ? storage === 'true' : true;
+
 document.documentElement.classList.toggle('dark', isDark);
 
 function toggleDarkMode() {
-document.documentElement.classList.toggle('dark');
-
-localStorage.setItem(
-  'darkMode',
-  document.documentElement.classList.contains('dark') ? 'true' : 'false'
-);
+ 
+  const currentDark = document.documentElement.classList.toggle('dark');
+  
+  localStorage.setItem('darkMode', currentDark ? 'true' : 'false');
 }
